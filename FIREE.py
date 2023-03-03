440c0a1aa79a71880d8313a93bd16c47db980592
TOXIC CYBER <85592998+Dark-Cyber-07@users.noreply.github.com>
2023-03-03 09:35:04 +0600

    Add files via upload

---
+++ FIREE.py
@@ -0,0 +1,32 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from X64_NUM import main
+ 
+        xyz()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from X32_NUM import main
+ 
+ 
+        xyz()
+ 
+
